aa7a30a70a8b3905f9135eae6f7d826b58d2c471
Felipe Kawassaki <fekawassaki@gmail.com>
2018-06-12 17:22:04 -0300

    Ajustes nos Icones | Animações | Rotas

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -18,28 +18,51 @@ export class MapComponent implements OnInit {
 
   private markers: any;
   private places: any;
-  private latitude: number;
-  private longitude: number;
+  private latitudeTo: number;
+  private longitudeTo: number;
   private zoom: number;
   private mapCenter: any;
-  private markerCurrentLocation = new google.maps.Marker();
-  // msgs: MessageModule[] = [];
-  // hidden: boolean= true;
   
+  private directionsService;
+  private directionsDisplay;
+
+  private markerCurrentLocation = new google.maps.Marker();
+  
+  private markerCurrentLocationInfo = new google.maps.InfoWindow({
+    content: 'Sua localização'
+  });
+
+  private circleCurrentPosition = {
+    path: google.maps.SymbolPath.CIRCLE,
+    fillColor: '#FF9E80',
+    fillOpacity: 1,
+    scale: 3,
+    strokeColor: '#FF6E40',
+    strokeWeight: 15,
+  };
+
+  private placeMarker = {
+    url: 'https://storage.googleapis.com/tcc-api-insight.appspot.com/map-marker.png',
+    size: new google.maps.Size(90, 90),
+    origin: new google.maps.Point(0, 0),
+    anchor: new google.maps.Point(17, 34),
+    scaledSize: new google.maps.Size(50, 50)
+  };
+
+
   constructor(
     private zone: NgZone,
   ) { }
 
   ngOnInit() {
     this.criaMapa();
-    this.markerCurrentLocation.setAnimation(google.maps.Animation.DROP);
     this.initAutocomplete();
     this.setLocalizacaoAtual();
   }
 
   criaMapa(){
     let mapProp = {
-      zoom: 17,
+      zoom: 15,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     };
     this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
@@ -47,7 +70,6 @@ export class MapComponent implements OnInit {
 
   setLocalizacaoAtual(){
     const self = this;
-
     const options = {
       enableHighAccuracy: false,
       timeout: 500,
@@ -62,24 +84,20 @@ export class MapComponent implements OnInit {
         self.map.setCenter(self.mapCenter);
       }
 
-      let contentString = 'Sua localização';
-
-      let infowindow = new google.maps.InfoWindow({
-        content: contentString
-      });
-
+      self.markerCurrentLocation.setAnimation(google.maps.Animation.DROP);
       self.markerCurrentLocation.setPosition(self.mapCenter);
       self.markerCurrentLocation.setMap(self.map);
       self.markerCurrentLocation.setDraggable(true);
+      self.markerCurrentLocation.setIcon(self.circleCurrentPosition);
 
       self.markerCurrentLocation.addListener('mouseover', function() {
-        infowindow.open(self.map, self.markerCurrentLocation);
-        self.toggleBounce(self.markerCurrentLocation)
+        self.markerCurrentLocationInfo.open(self.map, self.markerCurrentLocation);
       });
+
       self.markerCurrentLocation.addListener('mouseout', function(){
-        infowindow.close();
-        self.toggleBounce(self.markerCurrentLocation)
+        self.markerCurrentLocationInfo.close();
       });
+
       self.markerCurrentLocation.addListener('dragend', function(mark){
         let localationDraggend = new google.maps.LatLng(mark.latLng.lat(), mark.latLng.lng());
         if(self.mapCenter !== localationDraggend){
@@ -89,7 +107,7 @@ export class MapComponent implements OnInit {
         }
       });
     }, null, options);
-    
+
     if(self.markers){
       self.markers.push(self.markerCurrentLocation);
     }
@@ -140,6 +158,7 @@ export class MapComponent implements OnInit {
   getDetails() {
     var self = this;
     let service = new google.maps.places.PlacesService(this.map);
+    let timeout;
 
     self.places.forEach(function (placeEach) {
       service.getDetails({
@@ -147,40 +166,48 @@ export class MapComponent implements OnInit {
       }, function (place, status) {
 
         if (status === google.maps.places.PlacesServiceStatus.OK) {
-          let marker = self.addMarkPlace(place);
-          
-          google.maps.event.addListener(marker, 'click', function () {
-            console.log(self.markers);
-          });
+
+          for (var i = 0; i < placeEach.length; i++) {
+            timeout = i * 200;
+          }
+          window.setTimeout(function() {
+            let marker = self.addMarkPlace(place);
+
+            google.maps.event.addListener(marker, 'click', function () {
+
+              if(marker.getAnimation() === google.maps.Animation.BOUNCE){
+                marker.setAnimation(null);
+                self.latitudeTo = null;
+                self.longitudeTo = null;
+              } else {
+                marker.setAnimation(google.maps.Animation.BOUNCE);
+                self.latitudeTo = place.geometry.location.lat();
+                self.longitudeTo = place.geometry.location.lng();
+              }
+            });
+          }, timeout);
+
         }
       });
     });
   }
-
-
-  addMarkPlace(place){
+  addMarkPlace(place): any {
     var self = this;
     let bounds = new google.maps.LatLngBounds();
-    
+
     if (!place.geometry) {
       console.log("Returned place contains no geometry");
       return;
     }
-    var icon = {
-      url: place.icon,
-      size: new google.maps.Size(71, 71),
-      origin: new google.maps.Point(0, 0),
-      anchor: new google.maps.Point(17, 34),
-      scaledSize: new google.maps.Size(25, 25)
-    };
 
     let marker = new google.maps.Marker({
       map: self.map,
-      icon: icon,
+      icon: self.placeMarker,
       title: place.name,
-      position: place.geometry.location
+      position: place.geometry.location,
+      animation: google.maps.Animation.DROP
     })
-     
+
     self.markers.push(marker);
 
     if (place.geometry.viewport) {
@@ -189,37 +216,56 @@ export class MapComponent implements OnInit {
       bounds.extend(place.geometry.location);
     }
 
-    this.map.fitBounds(bounds);
+    self.map.fitBounds(bounds);
+    self.map.setZoom(13);
+    self.map.setCenter(self.mapCenter);
 
     return marker;
-  }
-
+}
   // getQuerySelector(query){
   //   this.hidden = true;
   //   console.log("map.component.ts", query);
   // }
 
   gerarRota(){
-    var directionsService = new google.maps.DirectionsService;
-    var directionsDisplay = new google.maps.DirectionsRenderer({
-      draggable: true,
-      map: this.map
-    });
+    var self = this;
+    if (self.directionsDisplay != null) {
+      self.directionsDisplay.setMap(null);
+      self.directionsDisplay = null;
+    }
 
-    this.calculateAndDisplayRoute(directionsService, directionsDisplay);
+    self.directionsService = new google.maps.DirectionsService;
+    self.directionsDisplay = new google.maps.DirectionsRenderer;
+    // polylineOptions: {
+    //   strokeColor: "red"
+    // }, suppressMarkers: true
+
+    self.directionsDisplay.setMap(self.map);
+    self.directionsDisplay.setOptions( { suppressMarkers: true } );
+    self.calculateAndDisplayRoute(self.directionsService, self.directionsDisplay);
   }
 
   calculateAndDisplayRoute(directionsService, directionsDisplay) {
-    directionsService.route({
-      origin: {lat: this.markerCurrentLocation.getPosition().lat(), lng: this.markerCurrentLocation.getPosition().lng()},
-      destination: {lat: -25.4226576, lng: -49.27069740000002},
-      travelMode: google.maps.TravelMode.DRIVING
-    }, function(response, status) {
-      if (status === 'OK') {
-        directionsDisplay.setDirections(response);
-      } else {
-        window.alert('Directions request failed due to ' + status);
-      }
-    });
+    var self = this;
+    directionsDisplay.setDirections({routes: []});
+    if(self.latitudeTo && self.longitudeTo){
+      directionsService.route({
+        origin: {lat: self.markerCurrentLocation.getPosition().lat(), lng: self.markerCurrentLocation.getPosition().lng()},
+        destination: {lat: self.latitudeTo, lng: self.longitudeTo},
+        travelMode: google.maps.TravelMode.DRIVING
+      }, function(response, status) {
+        if (status === 'OK') {
+          directionsDisplay.setDirections(response);
+        } else {
+          window.alert('Directions request failed due to ' + status);
+        }
+      });
+    } else {
+      console.log("Escolha um local para traçar a rota")
+    }
+  }
+
+  limparRotas(directionsDisplay){
+    directionsDisplay.setMap(null);
   }
 }
